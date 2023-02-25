2a296076d700ce136e986ba63c321f11257697a2
joaowozniak <joao.wozniak@gmail.com>
2023-02-25 22:01:10 +0000

    update request

--- match.js
+++ match.js
@@ -4,8 +4,8 @@ const matchInfo = document.getElementById("match-info");
 // Fetch the HTML content of the SL Benfica's calendar page
 fetch("https://www.slbenfica.pt/pt-pt/futebol/calendario", {
     headers: {
-        'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/58.0.3029.110 Safari/537.3',
-        'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,*/*;q=0.8'
+        "Content-Type": "application/json",
+        'user-agent': 'accept'
     }
 })
     .then(response => response.text())
