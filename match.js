554cea45613b7b8f0012518dc5013fdbde6ac970
joaowozniak <joao.wozniak@gmail.com>
2023-02-25 21:52:39 +0000

    update retrieval

--- match.js
+++ match.js
@@ -8,13 +8,26 @@ fetch("https://www.slbenfica.pt/pt-pt/futebol/calendario")
         // Load the HTML content into a Cheerio instance
         const $ = cheerio.load(html);
 
-        // Retrieve the HTML element that contains the next match information
-        const nextMatchElement = $('.sched-info').first();
+        // Find all the match elements on the page
+        const matchElements = $('.sched-info');
+
+        // Find the first match element with a date that is in the future
+        const nextMatchElement = matchElements.toArray().find(element => {
+            const dateStr = $(element).find('.sched-date').text();
+            const matchDate = new Date(dateStr);
+            return matchDate > new Date();
+        });
+
+        // If no upcoming matches were found, display a message
+        if (!nextMatchElement) {
+            matchInfo.textContent = "There are no upcoming matches.";
+            return;
+        }
 
         // Retrieve the date, opponent, and location of the next match
-        const date = nextMatchElement.find('.sched-date').text();
-        const opponent = nextMatchElement.find('.sched-vs').text();
-        const location = nextMatchElement.find('.sched-venue').text();
+        const date = $(nextMatchElement).find('.sched-date').text();
+        const opponent = $(nextMatchElement).find('.sched-vs').text();
+        const location = $(nextMatchElement).find('.sched-venue').text();
 
         // Update the HTML content of the "match-info" element with the retrieved match information
         matchInfo.textContent = `The next SL Benfica match is ${date} against ${opponent} at ${location}.`;
